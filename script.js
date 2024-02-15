4cc0c95c10248918b78772b32de3905d134c7579
Kudinov Andrej <kudinov.andrej@list.ru>
2024-02-16 01:43:48 +0300

    десктопная версия

--- script.js
+++ script.js
@@ -41,5 +41,24 @@
     updateCounter();
   }, 4000);
 
+  const links = document.querySelectorAll('.header__link');
+
+  links.forEach(link => {
+      link.addEventListener('click', smoothScroll);
+  });
+
+  function smoothScroll(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+          window.scrollTo({
+              top: targetElement.offsetTop,
+              behavior: 'smooth'
+          });
+      }
+  }
 
 
