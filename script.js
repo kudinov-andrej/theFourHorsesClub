d4e108a397a3da934df4d6579eec637bcf46fe64
Kudinov Andrej <kudinov.andrej@list.ru>
2024-02-16 00:04:53 +0300

    сделал блок с участниками турнира

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const list = document.querySelector('.players__list');
+  const listWrapper = document.querySelector('.players__list-wrapper');
+  const cardWidth = 394;
+  const gap = 20;
+  let currentIndex = 0;
+  const counter = document.querySelector('.players__counter_type_active');
+  const totalCount = list.children.length - 2;
+  
+  // Функция для обработки клика на кнопки
+  function handleButtonClick(direction) {
+    if (direction === 'left') {
+      currentIndex = Math.max(currentIndex - 1, 0);
+    } else if (direction === 'right') {
+      currentIndex = (currentIndex + 1) % totalCount;
+    }
+
+    updateListPosition();
+    updateCounter();
+  }
+
+  // Функция для обновления позиции списка
+  function updateListPosition() {
+    const newPosition = -(currentIndex * (cardWidth + gap));
+    list.style.transform = `translateX(${newPosition}px)`;
+   
+  }
+  
+  function updateCounter() {
+    counter.textContent = (currentIndex + 3).toString();
+  }
+
+  // Обработчики событий для кнопок
+  document.querySelector('.players__button_type_left').addEventListener('click', () => handleButtonClick('left'));
+  document.querySelector('.players__button_type_right').addEventListener('click', () => handleButtonClick('right'));
+
+   // Бесконечный скролл вправо каждые 4 секунды
+   setInterval(() => {
+    currentIndex = (currentIndex + 1) % totalCount;
+    updateListPosition();
+    updateCounter();
+  }, 4000);
+});
+
+
